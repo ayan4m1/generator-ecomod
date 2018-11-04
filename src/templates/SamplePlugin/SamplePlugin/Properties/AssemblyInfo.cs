297701e497fd2e976b8ea0b5fea784e20d6902d1
ayan4m1 <andrew@bulletlogic.com>
2018-11-04 17:52:56 -0500

    add sample VS solution

---
+++ src/templates/SamplePlugin/SamplePlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("<%= name %>")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("<%= name %>")]
+[assembly: AssemblyCopyright("Copyright © <%= author %> 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("<%= version %>")]
+[assembly: AssemblyFileVersion("<%= version %>")]
---
+++ src/templates/SamplePlugin/SamplePlugin/SamplePlugin.cs
@@ -0,0 +1,12 @@
+﻿using Eco.Core.Plugins.Interfaces;
+
+namespace <%= name %>
+{
+    public class <%= name %> : IModKitPlugin
+    {
+        public string GetStatus()
+        {
+            return GetType().FullName;
+        }
+    }
+}
